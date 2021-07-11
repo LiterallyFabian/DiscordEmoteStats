525eac6de5824143cd0433d546c6e0ab498fa74d
Fabian <lindgrenfabian@gmail.com>
2021-07-11 21:49:40 +0200

    add official/custom graph

--- js/emoji.js
+++ js/emoji.js
@@ -1,4 +1,7 @@
 var data = {};
+var sortedData = [];
+var totalEmoji = 0;
+var totalCustom = 0;
 
 function initToastr() {
     toastr.options = {
@@ -37,10 +40,49 @@ function submit() {
         toastr["error"]("The data could not be parsed correctly.", "Error");
         return;
     }
-
+    for (var key in data) {
+        sortedData.push({
+            key: key,
+            totalUses: data[key].totalUses,
+            score: data[key].score
+        })
+        totalEmoji++;
+        if (!isNaN(parseInt(key))) totalCustom++;
+    }
+    sortedData.sort((a, b) => (a.totalUses < b.totalUses) ? 1 : -1)
     generateStats();
+    $("#dataentry").css("display", "none");
 }
 
 function generateStats() {
 
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
+}
+
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Type', 'Count'],
+        ['Official', totalEmoji - totalCustom],
+        ['Custom', totalCustom]
+    ]);
+
+    var options = {
+        title: 'Total official vs custom emojis used',
+        slices: {
+            0: {
+                color: "#7289DA"
+            },
+            1: {
+                color: "#9CB0F8"
+            }
+        }
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
 }
